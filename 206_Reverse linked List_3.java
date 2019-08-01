e2b53b763c8f49b8dc1db5feab0c7ae5cf1cebbc
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-01 10:24:32 -0400

    Create 206_Reverse linked List_3.java

---
+++ 206_Reverse linked List_3.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+ 
+ // recursive construction
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if (head == null || head.next == null) return head;
+        
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        
+        return node;
+        
+    }
+}
