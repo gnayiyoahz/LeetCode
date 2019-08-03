ccf400b79a19595585bfdc31725b4980e1525599
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-02 22:17:26 -0400

    Create 26_Remove Duplicates from Sorted Array.c

---
+++ 26_Remove Duplicates from Sorted Array.c
@@ -0,0 +1,23 @@
+
+
+int removeDuplicates(int* nums, int numsSize){
+    
+    if (numsSize == 0) return 0;
+    
+    int i = 0, j = 1;
+    while (j < numsSize) {
+        if (nums[j] == nums[j-1]){
+            j++;
+        } else {
+            nums[i+1] = nums[j];
+            i++;
+            j++;
+        } 
+    }
+    return i+1;
+    
+    
+    
+    
+}
+
