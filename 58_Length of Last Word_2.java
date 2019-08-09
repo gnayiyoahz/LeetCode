b0c58f24650ee4687be7dda7387c7e23793062d4
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-09 14:47:21 -0400

    Create 58_Length of Last Word_2.java

---
+++ 58_Length of Last Word_2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int i = s.length()-1;
+        
+        if(i > -1) {
+            while(i > -1 && s.charAt(i) == ' ') i--;
+            while(i > -1 && s.charAt(i) != ' ' ) {
+                count++;
+                i--;
+            }
+        }
+
+        return count;
+    }
+}
