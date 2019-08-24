83072e0ada62cd05b9cf91b3c7e24762bc203ab4
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-24 00:18:44 -0400

    Update 104_Maximum Depth of Binary Tree.java

--- 104_Maximum Depth of Binary Tree.java
+++ 104_Maximum Depth of Binary Tree.java
@@ -10,7 +10,6 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
         if (root.left == null) return maxDepth(root.right)+1;
         if (root.right == null) return maxDepth(root.left)+1;
         return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
