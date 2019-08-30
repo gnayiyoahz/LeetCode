05f7628a9677afa22bf6d3337d830e6ecd5543fe
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-29 23:52:36 -0400

    Create 217_Contains_Duplicate_1.java

---
+++ 217_Contains_Duplicate_1.java
@@ -0,0 +1,11 @@
+// Using set
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (!set.add(n)) return true;
+        }
+        return false;
+    }
+}
