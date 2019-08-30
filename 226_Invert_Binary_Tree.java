148cdd0070afdfc17328612a69a7d319219cdc5b
gnayiyoahz <47898403+gnayiyoahz@users.noreply.github.com>
2019-08-30 01:03:45 -0400

    Update 226_Invert_Binary_Tree.java

--- 226_Invert_Binary_Tree.java
+++ 226_Invert_Binary_Tree.java
@@ -11,18 +11,8 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
         TreeNode temp = root.left;        
-        if (isLeaf(root.left) && isLeaf(root.right)) {
-            root.left = root.right;
-            root.right = temp;
-        } else {
-            root.left = invertTree(root.right);
-            root.right = invertTree(temp);
-        }
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
         return root;
-    }
-    
-    public boolean isLeaf(TreeNode node) {
-        if (node == null) return false;
-        return node.left == null && node.right == null;
     }
 }
